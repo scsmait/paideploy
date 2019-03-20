099fbe27d7743043adb53d4e6b02afb4bf1a7d60
scsmait <github@scsmait.com>
2019-03-20 13:11:33 +0800

    11111

--- script.sh
+++ script.sh
@@ -3,6 +3,8 @@
 # Insert code to fetch information to be returned, then write to stdout
 
 # write an array delimiter before and after the data we want
-echo '#DATA#'
-echo 'this is the droid you have been looking for'
-echo '#DATA#'
+
+sudo apt-get update
+sudo apt-get install docker.io
+sudo systemctl enable docker
+sudo systemctl restart docker.service
