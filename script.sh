8d88ed0e0d708c3aebc64c1e785dac5d39c7aa6b
scsmait <github@scsmait.com>
2019-03-20 14:27:14 +0800

    125

--- script.sh
+++ script.sh
@@ -4,7 +4,19 @@
 
 # write an array delimiter before and after the data we want
 
+# 安装docker
 sudo apt-get update
 sudo apt-get install docker.io
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
+
+# 增加nvidia-container-runtime repo地址
+curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | sudo apt-key add -
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list | sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
+sudo apt-get update
+
+# 添加所需依赖
+sudo apt-get install nvidia-container-runtime python
+
+
